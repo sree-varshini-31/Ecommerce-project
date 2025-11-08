94864eea9a36df5888e7872a3fdab83c9df331f9
sree-varshini-31 <varshini31.sree@gmail.com>
2025-11-08 23:20:41 +0530

    Fragrances

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,31 @@ import './index.css';
 import Navbar from './components/Navbar';
 import Hair from './components/Hair';
 import Home from './components/Home';
-<<<<<<< HEAD
+
 import Combos from './components/Combos';
 import Best from './components/Best';
-=======
+
 import Fragrances from './components/Fragrances';
 
 
->>>>>>> 969a9caab3a08ce1f274f75ea9de586428792f26
+
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-       {/* <Home/>  */}
-=======
-      <Home/>
->>>>>>> 969a9caab3a08ce1f274f75ea9de586428792f26
+
+     
+      {/* <Home/> */}
+
       {/* <Register /> */}
       {/* <Navbar/> */}
       {/* <Combos /> */}
       {/* <Hair /> */}
-<<<<<<< HEAD
-      <Best />
-=======
       
->>>>>>> 969a9caab3a08ce1f274f75ea9de586428792f26
+{/* <Fragrances /> */}
+      {/* <Best /> */}
+      
+
     </div>
   )
 }
